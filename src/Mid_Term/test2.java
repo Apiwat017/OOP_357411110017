6e3a4e4263fd7dff98d0f0e3c87d97c555193b3c
LAB_COM-MT_1 <krishna357411110017@gmail.com>
2018-01-19 13:35:16 +0700

    MidTerm test2

--- src/Mid_Term/test2.java
+++ src/Mid_Term/test2.java
@@ -1,5 +1,4 @@
 package Mid_Term;
-
 import java.util.Scanner;
 
 public class test2 {
@@ -9,63 +8,49 @@ public class test2 {
         int getSalary = 0;
         int net = 0;
         int salarynet = 0;
-        int getTime = 0;
+        int getOverTime = 0;
+
         Scanner oj = new Scanner(System.in);
-        System.out.print("\nรหัสพนักงาน : ");
+        System.out.print("\nรหัสพนักงาน: ");
         getID = oj.nextInt();
 
         Scanner Keyboard = new Scanner(System.in);
-        System.out.print("\nชื่อพนักงาน : ");
+        System.out.print("\nชื่อพนักงาน: ");
         getName = Keyboard.nextLine();
 
-        System.out.print("\nเงินเดือนพนักงาน : ");
+        System.out.print("\nเงินเดือนพนักงาน: ");
         getSalary = oj.nextInt();
 
-        System.out.print("\nค่าล่วงเวลา : ");
-        getTime = oj.nextInt();
+        System.out.print("\nค่าล่วงเวลา: ");
+        getOverTime = oj.nextInt();
 
-        if (getSalary < 30000)
-        {
-            net = (getSalary * 1) / 100;
-            salarynet=(getSalary+net);
-            System.out.println("\nเสียภาษี : 1% ");
-            System.out.println("\nภาษีที่เสีย :" + net);
-            int getSalry;
-            System.out.println("\nเงินเดือนสุทธิ :" +getTime);
-            System.out.println("");
-        }
-        else if (getSalary <= 30000)
-        {
-            net = (getSalary * 3) / 100;
-            salarynet = (getSalary + net);
-            System.out.println("\nเสียภาษี : 3% ");
-            System.out.println("\nภาษีที่เสีย :" + net);
-            System.out.println("\nงินเดือนสุทธิ : " + salarynet);
-        }
-        else if (getSalary <= 50000)
-        {
-            net = (getSalary * 5) / 100;
-            salarynet = (getSalary + net);
-            System.out.println("\nเสียภาษี : 5% ");
-            System.out.println("\nภาษีที่เสีย :" + net);
-            System.out.println("\nงินเดือนสุทธิ : " + salarynet);
-        }
-        else if (getSalary <= 70000)
-        {
-            net = (getSalary * 7) / 100;
-            salarynet = (getSalary + net);
-            System.out.println("\nเสียภาษี : 7% ");
-            System.out.println("\nภาษีที่เสีย :" + net);
-            System.out.println("\nงินเดือนสุทธิ : " + salarynet);
-        }
-        else if (getSalary >= 100000)
-        {
-            net = (getSalary * 10) / 100;
-            salarynet = (getSalary + net);
-            System.out.println("\nเสียภาษี : 10% ");
-            System.out.println("\nภาษีที่เสีย :" + net);
-            System.out.println("\nงินเดือนสุทธิ : " + salarynet);
-        }
+        if (getSalary+getOverTime >= 100000) {
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี: 10%");
+            System.out.println("\nเงินเดือนสุทธิ: " + net);
+            System.out.println("\nภาษี:" + net*10/100);
+        } else if ((getSalary+getOverTime >= 70000)&&(getSalary+getSalary <= 99999)) {
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี: 7%");
+            System.out.println("\nเงินเดือนสุทธิ: " + net);
+            System.out.println("\nภาษี:" + net*7/100);
+        } else if ((getSalary+getOverTime >= 50000)&&(getSalary+getSalary <= 69999)) {
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี: 5%");
+            System.out.println("\nเงินเดือนสุทธิ: " + net);
+            System.out.println("\nภาษี:" + net*5/100);
+        }else if ((getSalary+getOverTime >= 30000)&&(getSalary+getOverTime <= 49999)) {
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี: 3%");
+            System.out.println("\nเงินเดือนสุทธิ: " + net);
+            System.out.println("\nภาษีที่ต้องเสีย:" + net*3/100);
 
+        }
+        else if (getSalary+getOverTime < 29999){
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี:1%");
+            System.out.println("\nเงินเดือนสุทธิ: "+ net);
+            System.out.println("\nภาษีที่ต้องเสีย:" +net*1/100);
+        }
     }
 }

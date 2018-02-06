5dd8328b449599c92f981c33ba39f11572b10dc7
Apiwat017 <krishna357411110017@gmail.com>
2018-02-06 10:49:16 +0700

    oop_lab9

--- src/oop_lab8/PetApp.java
+++ src/oop_lab8/PetApp.java
@@ -1,13 +1,22 @@
 package oop_lab8;
 
+import oop_lab9.Cow;
+
 public class PetApp {
     public static void main(String[] args) {
         Dog dog = new Dog("Chayen",2,"Bangkeaw");
         System.out.println(dog.toString());
         dog.makeNoise();
+        dog.myPet();
 
         cat cat = new cat("Giant",2,"Persian");
         System.out.println(cat.toString());
         cat.makeNoise();
-    }
-}
+        cat.myPet();
+
+        Cow cow = new Cow("Nmajai",5);
+        System.out.println(cow.toString());
+        cow.makeNoise();
+        cow.myPet();
+    }//main
+}//class
---
+++ src/oop_lab9/PersonApp.java
@@ -0,0 +1,11 @@
+package oop_lab9;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Address address = new Address("12/22","Nakhonsithammarat","80110");
+        Job job = new Job("police",20000);
+        Person person = new Person("357411110017","Apiwat",address,job);
+
+        System.out.println(person.toString());
+    }
+}//class
